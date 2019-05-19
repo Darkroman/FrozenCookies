db274ba62caa4d5aa9700ae01c27ba1984d24bd6
Darkroman <roman8707@yahoo.com>
2019-05-19 14:26:39 -0700

    Fixed typo of repository name

--- frozen_cookies.js
+++ frozen_cookies.js
@@ -4,7 +4,7 @@ var scriptElement = document.getElementById('frozenCookieScript') !== null ?
     document.getElementById('modscript_frozen_cookies');
 var baseUrl = scriptElement !== null ?
     scriptElement.getAttribute('src').replace(/\/frozen_cookies\.js$/, '') :
-    'https://Darkroman.github.io/FrozenCookiess/';
+    'https://darkroman.github.io/FrozenCookies/';
 var FrozenCookies = {
     'baseUrl': baseUrl,
     'branch': '',
