934f019428bf0acf0eb29e414bf06bd47ee288c1
Darkroman <roman8707@yahoo.com>
2019-05-19 15:06:57 -0700

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -411,7 +411,7 @@ function updateTimers() {
 }
 
 function FCMenu() {
-    	Game.UpdateMenu;
+    	Game.UpdateMenu = function() {
         var currentCookies, maxCookies, isTarget, isMax, targetTxt, maxTxt,
             currPrestige, resetPrestige, prestigeDifference,
             currHC, resetHC, cps, baseChosen, frenzyChosen, clickStr, buildTable,
@@ -504,7 +504,7 @@ function FCMenu() {
                 subsection.append($('<div>').addClass('listing').html('<b>Max HC Gain/hr:</b> ' + Beautify(FrozenCookies.maxHCPercent)));
             }
             subsection.append($('<div>').addClass('listing').html('<b>Average HC Gain/hr:</b> ' + Beautify(60 * 60 * (FrozenCookies.lastHCAmount - currHC)/((FrozenCookies.lastHCTime - Game.startDate)/1000))));
-        
+        }
         menu.append(subsection);
 
         // Harvesting
