89043f5686fcb308924250721c9a1b023b5e7bde
Darkroman <roman8707@yahoo.com>
2019-05-19 17:37:04 -0700

    Update fc_button.js

--- fc_button.js
+++ fc_button.js
@@ -511,8 +511,10 @@ function FCMenu() {
             subsection.append($('<div>').addClass('listing').html('<b>Average HC Gain/hr:</b> ' + Beautify(60 * 60 * (FrozenCookies.lastHCAmount - currHC)/((FrozenCookies.lastHCTime - Game.startDate)/1000))));
 	}
         menu.append(subsection);
-	setTimeout(Game.UpdateMenu, 500);
-
+//	TESTING
+//	setTimeout(Game.UpdateMenu, 500);
+	Game.UpdateMenu;
+//	END TESTING
         // Harvesting
 	if (FrozenCookies.setHarvestBankPlant){
 	    subsection = $('<div>').addClass('subsection');
