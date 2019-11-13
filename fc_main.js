1c2915d9d6b03a4006bf7900cf756c5d4b6f6b54
Darkroman <roman8707@yahoo.com>
2019-11-12 17:13:04 -0800

    Update fc_main.js

--- fc_main.js
+++ fc_main.js
@@ -2846,7 +2846,7 @@ function FCStart() {
     }
 	
 	if (FrozenCookies.autoSL) {
-    FrozenCookies.autoSLBot = setInterval(autoRigidel, FrozenCookies.frequency*2)
+    FrozenCookies.autoSLBot = setInterval(autoRigidel, FrozenCookies.frequency*10)
     }
 
     if (statSpeed(FrozenCookies.trackStats) > 0) {
